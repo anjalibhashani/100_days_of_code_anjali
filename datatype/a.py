d33c3a8a9e25719ba4dad3f29b990f284b878977
anjalibhashani <42154316+anjalibhashani@users.noreply.github.com>
2018-09-19 00:18:59 +0530

    added new project

---
+++ datatype/a.py
@@ -0,0 +1,6 @@
+#tuples
+tup=(21,"anajli",20.3);
+print (tup);
+tup+(54,78);
+print(tup);
+print tup+(54,88,"sonal");
---
+++ datatype/c.py
@@ -0,0 +1,9 @@
+#dictionaries
+mydic={1:'josh',3:'james',3:'raghu'};
+print (mydic);
+print (mydic[3]);
+#print (mydic['raghu']);//error
+print('raghu');
+print (mydic.keys());
+print (mydic.values());
+print (mydic.pop(1));
