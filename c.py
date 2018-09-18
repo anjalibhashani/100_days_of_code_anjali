ad7f4642f30c87722e4ef2fe95f725274d6c0d15
anjalibhashani <42154316+anjalibhashani@users.noreply.github.com>
2018-09-19 00:16:18 +0530

    Delete c.py

--- c.py
+++
@@ -1,9 +0,0 @@
-#dictionaries
-mydic={1:'josh',3:'james',3:'raghu'};
-print (mydic);
-print (mydic[3]);
-#print (mydic['raghu']);//error
-print('raghu');
-print (mydic.keys());
-print (mydic.values());
-print (mydic.pop(1));
